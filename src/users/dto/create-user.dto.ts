1c60f114f61b4511f2d6bad7441205cbd03275f6
Brian Valdivieso <brian.valdivieso.dev@gmail.com>
2023-12-02 07:36:35 +0100

    Add swagger and husky

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,16 +1,21 @@
 import { IsNotEmpty } from 'class-validator';
 import { UserRole } from '../entity/users.entity';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({ enum: Object.values(UserRole) })
   @IsNotEmpty()
   rol: UserRole;
 }
